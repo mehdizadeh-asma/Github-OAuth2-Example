feee640e08e0577f8ecda699f34a713557e0640d
Asma Mehdizadeh <mehdizadeh.asma@gmail.com>
2023-01-07 20:27:48 +0330

    initial config of main middlewares

---
+++ Backend/src/app.ts
@@ -0,0 +1,46 @@
+import express, {
+  NextFunction,
+  Request,
+  Response,
+  ErrorRequestHandler,
+} from "express";
+import bodyParser from "body-parser";
+
+import userRouter from "./routes/UserRouter";
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use("/api", userRouter);
+
+app.use((_req: Request, res: Response, next: NextFunction) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+
+  next();
+});
+
+app.use(
+  (
+    err: ErrorRequestHandler,
+    _req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    console.log(err); //replace with logger later
+    if (res.headersSent) {
+      return next(err);
+    }
+    res.status(500);
+    res.json({ error: err });
+  }
+);
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({ message: "app is running!" });
+});
+
+app.listen(8080);
+console.log(`Server is Startig at http://localhost:8080`);
