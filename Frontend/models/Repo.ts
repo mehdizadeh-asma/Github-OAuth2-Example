cc032bc5810eb5f486d0fef0c6d0f59c70b66e7a
Asma Mehdizadeh <mehdizadeh.asma@gmail.com>
2023-01-12 12:17:42 +0330

    Implement models

---
+++ Frontend/models/Repo.ts
@@ -0,0 +1,15 @@
+interface Repo {
+  id: string;
+  name: string;
+  full_name: string;
+  description: string;
+  visibility: string; //public,private
+  html_url: string;
+  forks_count: string;
+  watchers_count: string;
+  stargazers_count: string;
+  language: string;
+  created_at: string;
+  updated_at: string;
+}
+export default Repo;
