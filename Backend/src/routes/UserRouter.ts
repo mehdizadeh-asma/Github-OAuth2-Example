b9233b79a7286f3c0c67a8a9b2afb874c3470965
Asma Mehdizadeh <mehdizadeh.asma@gmail.com>
2023-01-12 12:00:06 +0330

    Implement Authentication

--- Backend/src/routes/UserRouter.ts
+++ Backend/src/routes/UserRouter.ts
@@ -2,13 +2,14 @@ import { Router } from "express";
 
 import UserController from "../controller/UserController";
 
-
 const router = Router();
 
-router.post("/authorizeUser", UserController.authorizeUser);
+router.get("/authorizeuser/:code", UserController.authorizeUser);
+router.get("/user", UserController.GetAuthenticatedUser);
 router.get("/user/:username", UserController.GetUser);
 router.get("/gists/:username", UserController.GetGists);
 router.get("/repos/:username", UserController.GetRepos);
 router.get("/orgs/:username", UserController.GetOrgs);
+
 
 export default router;
